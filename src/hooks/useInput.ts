793e431d0fad45dc7d2044450135da195756c2b5
junguk11 <kjuzxcvbnm@naver.com>
2022-07-10 22:48:07 +0900

    upadte. 로그인/ 회원가입 CSS 1차 완료

--- src/hooks/useInput.ts
+++ src/hooks/useInput.ts
@@ -1,11 +1,21 @@
-import { Dispatch, SetStateAction, useCallback, useState, ChangeEvent } from 'react';
+import {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useState,
+  ChangeEvent,
+} from "react";
 
-type ReturnTypes<T> = [T, (e: ChangeEvent<HTMLInputElement>) => void, Dispatch<SetStateAction<T>>];
+type ReturnTypes<T> = [
+  T,
+  (e: ChangeEvent<HTMLInputElement>) => void,
+  Dispatch<SetStateAction<T>>
+];
 
 const useInput = <T>(initialData: T): ReturnTypes<T> => {
   const [value, setValue] = useState(initialData);
   const handler = useCallback((e: ChangeEvent<HTMLInputElement>) => {
-    setValue((e.target.value as unknown) as T);
+    setValue(e.target.value as unknown as T);
   }, []);
   return [value, handler, setValue];
 };
