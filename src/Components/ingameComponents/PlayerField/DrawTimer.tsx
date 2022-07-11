1d81ffa4601ae67958917e1beecd26e70dc0c615
kordobby <poordobby@naver.com>
2022-07-12 00:03:16 +0900

    add. timer 추가

---
+++ src/Components/ingameComponents/PlayerField/DrawTimer.tsx
@@ -0,0 +1,18 @@
+import { TimerWrap } from "../InGameStyled";
+
+const DrawTimer = () => {
+  return (
+    <TimerWrap>
+      <div className="circle_progress">
+        <div className="left">
+          <div className="bar"></div>
+        </div>
+        <div className="right">
+          <div className="bar"></div>
+        </div>
+      </div>
+    </TimerWrap>
+  );
+};
+
+export default DrawTimer;
