be574f62b6cd1b6c8dad6636202573734458f410
kordobby <poordobby@naver.com>
2022-07-25 11:57:48 +0900

    update. 카드 디자인 추가에 따른 카드 팩토리 작성

--- src/Components/ingameComponents/InGameStyled/CardFactory.tsx
+++ src/Components/ingameComponents/InGameStyled/CardFactory.tsx
@@ -1,10 +1,60 @@
-import one from "../../../Public/Images/1.png";
-
-export default function matchCardImg(data: number) {
+import curse from "../../../images/cardsBack/curseBack.png";
+import attack from "../../../images/cardsBack/attackBack.png";
+import item from "../../../images/cardsBack/itemBack.png";
+import enchant from "../../../images/cardsBack/enchantBack.png";
+export default function matchCardImg(data: string) {
   switch (data) {
-    case 1:
-      return one;
+    case "boulderStrike":
+      return attack;
+    case "deathRay":
+      return attack;
+    case "fireBall":
+      return attack;
+    case "magicMissile":
+      return attack;
+    case "manaSiphon":
+      return attack;
+    case "poisonArrow":
+      return attack;
+    case "mute":
+      return curse;
+    case "petrification":
+      return curse;
+    case "weaknessExposure":
+      return curse;
+    case "venom":
+      return curse;
+    case "yfeputs":
+      return curse;
+    case "sleep":
+      return curse;
+    case "magicAttenuation":
+      return curse;
+    case "beerMug":
+      return item;
+    case "leftOverOctopus":
+      return item;
+    case "manaPotion":
+      return item;
+    case "panacea":
+      return item;
+    case "channelingMana":
+      return enchant;
+    case "heal":
+      return enchant;
+    case "partyHeal":
+      return enchant;
+    case "resistance":
+      return enchant;
+    case "shield":
+      return enchant;
+    case "dispel":
+      return enchant;
+    case "magicAmplification":
+      return enchant;
+    case "magicArmor":
+      return enchant;
     default:
-      return one;
+      return enchant;
   }
 }
