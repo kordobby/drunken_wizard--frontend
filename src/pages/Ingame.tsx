9ed030aa7bfb359860616e8b5e0c1fb0e6ddd1d6
kordobby <poordobby@naver.com>
2022-08-01 11:03:36 +0900

    fix. game bug(nowPlayerID)

--- src/pages/Ingame.tsx
+++ src/pages/Ingame.tsx
@@ -160,7 +160,6 @@ const Ingame = () => {
             const msgData = JSON.parse(response?.content);
             const msgSender = response?.sender;
             const playersInfo = msgData.players;
-            dispatch(setNowPlayerIdTK(0));
             switch (response?.type) {
               case "START":
                 stompClient.unsubscribe(`/sub/wroom/${roomId}`);
